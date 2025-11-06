020d811fcbbd9a7214b88d5be5b119c9b5e4f511
RubanZargar <rubanzargar56@gmail.com>
2025-11-06 11:03:40 +0530

    Add

---
+++ call by reference.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+ void swapReference(int &x,int &y){
+ 	int temp=x;
+ 	x=y;
+ 	y=temp;
+ }
+ int main(){
+ 	int a=5,b=10;
+ 	cout<<"Before swap:a="<<a<<",b="<<b<<endl;
+ 	swapReference(a,b);
+	cout<<"After swap:a="<<a<<",b="<<b<<endl;
+	return 0;
+	
+ }
+ 
