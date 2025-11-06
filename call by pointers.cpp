32b8ad7dbae16687e139e62c3a160d1e9b798a62
RubanZargar <rubanzargar56@gmail.com>
2025-11-06 11:03:22 +0530

    Add

---
+++ call by pointers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void swapPointer(int *x , int *y){
+	int temp=*x;
+	*x=*y;
+	*y=temp;
+
+}
+int main(){
+	int a=5,b=10;
+	cout<<"Before swap: a="<<a<<",b="<<b<<endl;
+	swapPointer(&a,&b);
+	cout<<"After swap :a="<<a<<",b="<<b<<endl;
+	return 0;
+}
