25908fde20334d5e2c5c8af027b8ca8f5676edbd
RubanZargar <rubanzargar56@gmail.com>
2025-12-21 23:21:15 +0530

    Add

---
+++ constructorOrder.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Base
+{
+public:
+    Base()
+    {
+        cout << "Base class constructor called" << endl;
+    }
+
+    ~Base()
+    {
+        cout << "Base class destructor called" << endl;
+    }
+};
+
+class Derived : public Base
+{
+public:
+    Derived()
+    {
+        cout << "Derived class constructor called" << endl;
+    }
+
+    ~Derived()
+    {
+        cout << "Derived class destructor called" << endl;
+    }
+};
+
+int main()
+{
+    cout << "Object creation starts" << endl;
+    Derived obj;
+    cout << "Object destruction starts" << endl;
+
+    return 0;
+}
